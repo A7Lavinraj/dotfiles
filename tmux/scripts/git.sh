38417849f847f481a142a16c53dab3954a55c9d1
A7Lavinraj <lavinrajmohan@gmail.com>
2025-03-24 13:53:50 +0530

    chore: update tmux config

--- tmux/scripts/git.sh
+++ tmux/scripts/git.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 git_status() {
-  local branch dirty added modified deleted
+  local branch remote added modified deleted
   branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null) || return
+  remote=$(git remote get-url origin 2>/dev/null | sed -E 's#(git@|https://)[^/:]+[:/]##; s#\.git$##')
 
   added=$(git diff --numstat | awk '{s+=$1} END {print s}')
   modified=$(git diff --name-only | wc -l)
   deleted=$(git diff --numstat | awk '{s+=$2} END {print s}')
 
   if [[ -z "$added" && -z "$modified" && -z "$deleted" ]]; then
-    echo "#[fg=green] $branch"
+    echo "#[fg=green] $branch #[fg=green]($remote)"
   else
-    echo "#[fg=grey] $branch #[fg=yellow] ${added:-0} #[fg=blue] ${modified:1} #[fg=red] ${deleted:-0}"
+    echo "#[fg=grey] $branch #[fg=green]($remote) #[fg=green] ${added:-0} #[fg=blue] ${modified:-0} #[fg=red] ${deleted:-0}"
   fi
 }
 
